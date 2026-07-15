6812313eb877cbf79b03f56ee9196ac6a8e974da
Sanne-BP <s.b.puister@student.rug.nl>
2026-07-15 20:18:29 +0200

    new script for exploring sub question 2

---
+++ Results/02-SubQuestion2.R
@@ -0,0 +1,2 @@
+#Sub Question 2: between site comparison after installment
+#After instalment, how do fish communities differ between modified revetments, control revetments, and natural rocky shores?
